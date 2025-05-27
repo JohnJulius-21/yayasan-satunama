cdf63c670195f8ae0ecf114698b4306081c22994
John Julius Pattikaihatu <john.julius@ti.ukdw.ac.id>
2025-05-27 13:23:33 +0700

    file_name in peserta

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -705,7 +705,7 @@ class TrainingController extends Controller
         // Ambil semua file dari reguler_files yang sesuai dengan id_reguler
         $files = DB::table('reguler_files')
             ->where('id_reguler', $id)
-            ->get(['file_url', 'filename']); // Ambil semua file_url
+            ->get(['file_url', 'file_name']); // Ambil semua file_url
 
         return view('user.training.pelatihan.reguler.materi', compact('reguler', 'files'), [
             'title' => 'Materi Pelatihan ' . $reguler->nama_pelatihan,
