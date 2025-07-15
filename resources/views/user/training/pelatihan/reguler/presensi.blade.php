78da1468a0a5b63659d36ab5f3bcde47e20fafb0
John Julius Pattikaihatu <john.julius@ti.ukdw.ac.id>
2025-07-15 17:51:19 +0700

    fix presensi reguler

--- resources/views/user/training/pelatihan/reguler/presensi.blade.php
+++ resources/views/user/training/pelatihan/reguler/presensi.blade.php
@@ -15,12 +15,12 @@
 @section('content')
     <div class="page-title">
         <div class="container d-lg-flex justify-content-between align-items-center">
-            <h1 class="mb-2 mb-lg-0">{{ $permintaan->nama_pelatihan }}</h1>
+            <h1 class="mb-2 mb-lg-0">{{ $reguler->nama_pelatihan }}</h1>
             <nav class="breadcrumbs">
                 <ol>
-                    <li><a href="{{ route('permintaan.pelatihan') }}">Pelatihan Saya</a></li>
+                    <li><a href="{{ route('reguler.pelatihan') }}">Pelatihan Saya</a></li>
                     <li><a
-                            href="{{ route('permintaan.pelatihan.list', $reguler->nama_pelatihan) }}">{{ $reguler->nama_pelatihan }}</a>
+                            href="{{ route('reguler.pelatihan.list', $reguler->nama_pelatihan) }}">{{ $reguler->nama_pelatihan }}</a>
                     </li>
                     <li class="current">Presensi </li>
                 </ol>
